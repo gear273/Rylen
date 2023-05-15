b6fffbc3c78696dddd2023e85d728567607f0d3c
Pixer9 <117622509+Pixer9@users.noreply.github.com>
2023-05-14 21:07:09 -0500

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -8,6 +8,7 @@ points_headers = {
     'Accept': 'application/geo+json'
     }
 
+# Get latitude and longitude of desired location [city, state]
 def geocode(client_location):
     geolocator = Nominatim(user_agent=points_headers['User-Agent'])
     location = geolocator.geocode(client_location)
@@ -16,8 +17,9 @@ def geocode(client_location):
     loc = [location.latitude, location.longitude]
     return loc
 
+# Query weather.gov API for information related to lat/lon, retrieved as json
 def get_gridpoints(location):
-    client_location = geocode(location)
+    client_location = geocode(location) # retrieve lat/lon data
     try:
         print("https://api.weather.gov/points/{0[0]},{0[1]}".format(client_location))
         points = requests.get(url="https://api.weather.gov/points/{0[0]},{0[1]}".format(client_location), headers=points_headers)
@@ -27,6 +29,7 @@ def get_gridpoints(location):
         return None
     return url
 
+# Query weather.gov API for forecast using url obtained from previous API call
 def getForecast(location):
     forecast_url = get_gridpoints(location)
     print(forecast_url)
@@ -40,6 +43,7 @@ def getForecast(location):
     forecast_data = json_data['properties']['periods']
     return forecast_data
 
+# Query weather.gov API for hourly forecast using appended url obtain from previous API call
 def getForecastHourly(location):
     forecast_url = get_gridpoints(location)
     print(forecast_url)
@@ -53,6 +57,7 @@ def getForecastHourly(location):
     forecast_data = json_data['properties']['periods']
     return forecast_data
 
+# Query weather.gov API for ALL active weather alerts for the passed state - ex. TX
 def getAllAlerts(location):
     try:
         url_data = requests.get(url="https://api.weather.gov/alerts/active/area/{0}".format(location), headers=points_headers)
@@ -62,8 +67,9 @@ def getAllAlerts(location):
         return None
     return alert_data
 
+# Query weather.gov API for specific county active weather alerts for passed city, state - ex. Dallas, TX
 def getCountyAlerts(location):
-    client_location = geocode(location)
+    client_location = geocode(location) # retrieve lat/lon data
     try:
         points = requests.get(url="https://api.weather.gov/points/{0[0]},{0[1]}".format(client_location), headers=points_headers)
         package = json.loads(points.text)
